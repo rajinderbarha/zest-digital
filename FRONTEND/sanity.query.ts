e657aeb6d7c5507bd8c7773463912530b42d5158
rajinderbarha <rajinderbarha@gmail.com>
2024-04-02 18:57:24 +0530

    working on about page

--- FRONTEND/sanity.query.ts
+++ FRONTEND/sanity.query.ts
@@ -50,34 +50,56 @@ export function getFooter(){
   )
 }
 
-export function getAbout(){
-  return client.fetch(
-    groq`
-    *[_type == "footer"] {
-      climate_actionImg,
-      earth_img,
-      smallDescription,
-      logo,
-      footerItem[] {
-        heading,
-        list[] {
-          name,
-          link,
-          icon
-        }
-      },
-      brands[] {
-        brandImage
-      },
-      social[] {
-        socialImage,
-        socialLink
-      },
-      copyRightText
+
+
+
+export async function getAboutData() {
+  const headerquery = `
+  *[_type == 'about'] {
+    hero {
+      title,
+      heading,
+      image
+    },
+    team[] {
+      image,
+      title,
+      description
+    },
+    ourValue {
+      heading,
+      ourValuesection[] {
+        image,
+        title,
+        description
+      }
+    },
+    lifeSection {
+      image,
+      title,
+      description
+    },
+    card[] {
+      image,
+      title,
+      description
+    },
+    escape {
+      heading,
+      description,
+      marquee[] {
+        title,
+        description
+      }
+    },
+    banner {
+      image1,
+      image2,
+      description
     }
-    
-    `
-  )
+  }
+  
+  `;
+  const data = await client.fetch(headerquery);
+  return data;
 }
-
-
