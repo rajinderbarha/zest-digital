b2d4b403ce10c6b2fa7913f15c519c934444df35
tejindersinghts75 <50661634+rajinderbarha@users.noreply.github.com>
2024-04-16 18:43:57 +0530

    set the design of growth pages, single case study

--- FRONTEND/src/components/GrowthEnginePage/GrowthEnginePage_Banner/index.tsx
+++ FRONTEND/src/components/GrowthEnginePage/GrowthEnginePage_Banner/index.tsx
@@ -7,19 +7,19 @@ const GrowthEnginePage_Banner = ({ data }: any) => {
 
   return (
     <>
-      <div className={`${classes.Singalcasebanner}`}>
+      <div className={`${classes.Singalcasebanner} main_padding xl:mt-[143px] lg:mt-[110px] md:mt-[80px] sm:mt-[50px] mt-[30px]`}>
         <div className="main_container">
           <div className="grid  max-w-[992px]">
             <div className="">
-              <h2 className="md:text-45px text-30px font-semibold font-mono bg-color-7 w-max px-2 mb-9">
+              <h2 className="xl:text-45px lg:text-40px md:text-35px sm:text-[28px] text-[22px] font-semibold font-mono bg-color-7 w-max px-2 lg:mb-[36px] md:mb-[20px] sm:mb-[10px] mb-[7px]">
                 {data.heading}
               </h2>
-              <h1 className="font-mono font-semibold md:text-55px text-35px mb-5 ">
+              <h1 className="font-mono font-semibold xl:text-55px lg:text-50px md:text-40px sm:text-30px text-[25px] mb-5 ">
                 {data.description}
               </h1>
               <Link
                 href={data.buttonLink}
-                className="font-mono text-base px-21px h-10 text-white bg-color-1 rounded-full  hover:text-color-1 hover:bg-white hover:border-white border border-color-1 "
+                className="font-mono text-base px-21px py-[5px] text-white bg-color-1 rounded-full  hover:text-color-1 hover:bg-white border border-color-1 "
               >
                 {data.buttonName}
               </Link>
--- FRONTEND/src/components/GrowthEnginePage/Growths/index.tsx
+++ FRONTEND/src/components/GrowthEnginePage/Growths/index.tsx
@@ -7,7 +7,7 @@ function Growths({ data }: { data: any }) {
     <>
       {data.length > 0 && (
         <div className="Growths max-w-[1365px] mx-auto px-5 mt-[70px] overflow-hidden">
-          <div className="2xl:mb-[300px] lg:mb-[200px] mb-[100px]">
+          <div className="2xl:mb-[235px] xl:mb-[200px] lg:mb-[175px] md:mb-[120px] sm:mb-[100px] mb-[50px]">
             {data.map((item: any, index: number) => (
               <h2
                 key={`${index}_Growths`}
@@ -20,7 +20,7 @@ function Growths({ data }: { data: any }) {
               <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-[1345px] mx-auto">
                 {data.slice(0, 2).map((item: any, index: number) => {
                   return (
-                    <div key={index}>
+                    <div key={index} className="grid">
                       <Growth_engine
                         props={{
                           title: item.card.heading,
