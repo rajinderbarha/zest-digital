9a95bd3d21189279f74f451d688bbfe68d3b99a1
tejindersinghts75 <50661634+rajinderbarha@users.noreply.github.com>
2024-07-05 12:23:15 +0530

    make marquee lazy load on iphone, careers page animation, calculator step5 add  radio btn

--- FRONTEND/src/common/Logo_moving/index.tsx
+++ FRONTEND/src/common/Logo_moving/index.tsx
@@ -9,11 +9,11 @@ const Logo_moving = ({ LogoMovdata }: LogoMoving) => {
   return <div
     className={`${classes.margin_top} text-white mx-auto max-w-[1720px] w-full  my-10`}
   >
-    <div className={`${classes.gradient_border} relative xl:py-16 lg:py-[50px] md:py-[40px] sm:py-[30px] py-[30px]`}>
+    <div className={`${classes.gradient_border} relative xl:py-16 lg:py-[50px] md:py-[40px] sm:py-[30px] py-[30px] overflow-hidden`}>
       <div className={`${classes.shadow_custom} `}></div>
 
-      <Marquee className={`xl:gap-[100px] lg:gap-[80px] md:gap-[60px] sm:gap-[40px] gap-[35px] `}>
-        <div className="flex  max-w-auto xl:gap-[100px] lg:gap-[80px] md:gap-[60px] sm:gap-[40px] gap-[35px] ">
+      <Marquee className={`marquee_div xl:gap-[100px] lg:gap-[80px] md:gap-[60px] sm:gap-[40px] gap-[35px] `}>
+        <div className="marquee_images flex  max-w-auto xl:gap-[100px] lg:gap-[80px] md:gap-[60px] sm:gap-[40px] gap-[35px] ">
           {LogoMovdata?.map((item: any, index: number) => {
             return (
               <Image
@@ -22,7 +22,8 @@ const Logo_moving = ({ LogoMovdata }: LogoMoving) => {
                 width={200}
                 height={81}
                 key={index}
-                className="xl:w-[200px] xl:h-[81px] lg:w-[180px] lg:h-[73px] md:w-[150px] md:h-[61px] sm:w-[120px] sm:h-[49px] w-[100px] h-[41px]  "
+                className="xl:w-[200px] xl:h-[81px] lg:w-[180px] lg:h-[73px] md:w-[150px] md:h-[61px] sm:w-[120px] sm:h-[49px] w-[100px] h-[41px]  " 
+                loading="eager"
               />
             );
           })}
@@ -36,6 +37,7 @@ const Logo_moving = ({ LogoMovdata }: LogoMoving) => {
                 height={81}
                 key={index}
                 className="xl:w-[200px] xl:h-[81px] lg:w-[180px] lg:h-[73px] md:w-[150px] md:h-[61px] sm:w-[120px] sm:h-[49px] w-[100px] h-[41px]  "
+                  loading="eager"
               />
             );
           })}
--- FRONTEND/src/components/Resources_Calculater_Step_5/index.tsx
+++ FRONTEND/src/components/Resources_Calculater_Step_5/index.tsx
@@ -2,6 +2,7 @@ import ProgressBar from '@/common/ProgressBar';
 import { useState } from 'react'
 import { GoArrowLeft } from "react-icons/go";
 import { useRouter } from 'next/router';
+import classes from "../Resources_Calculater_Step_1/Resources_Calculater.module.css"
 
 function Resources_Calculater_Step_5({ setStep, setFormData, sendDataToGoogleSheets, downloadUrls }: any) {
     const [userEmail, setUserEmail] = useState("");
@@ -52,7 +53,7 @@ function Resources_Calculater_Step_5({ setStep, setFormData, sendDataToGoogleShe
 
 
 
-    return <div className="Resources_Calculater_Step_5">
+    return <div className={`${classes.Resources_Calculater_Step_5} Resources_Calculater_Step_5`}>
         <div className="pe-[6px] pb-[6px]">
             <div className="Resources_Calculater_Step_5 popup_conatiner md:pt-[29px] sm:pt-[20px] pt-[12px] lg:pb-[66px] md:pb-[55px] sm:pb-[40px] pb-[25px] xl:px-[50px] lg:px-[40px] md:px-[30px] sm:px-[20px] px-[10px] relative bg-white">
                 <div className="progrees_bar xl:mb-[123px] lg:mb-[100px] md:mb-[80px] sm:mb-[70px] mb-[60px] ">
@@ -64,17 +65,17 @@ function Resources_Calculater_Step_5({ setStep, setFormData, sendDataToGoogleShe
                     <div className="md:mb-[110px] sm:mb-[80px] mb-[60px]">
                         <p className="text-[15px] sm:text-base font-mono font-semibold md:mb-[10px] mb-[7px]">Communication preferences</p>
                         <div className="">
-                            <input type="checkbox" name="confirm1" id="confirm1" />
+                            <input type="radio" name="confirm1" id="confirm1"  />
                             <label htmlFor="confirm1" className='ml-[8px] font-lato sm:text-lg text-base font-normal'>Yes, please send me more tools and exclusive bonuses</label>
                         </div>
                         <div className="">
-                            <input type="checkbox" name="confirm2" id="confirm2" />
+                            <input type="radio" name="confirm1" id="confirm2"  />
                             <label htmlFor="confirm2" className='ml-[8px] font-lato sm:text-lg text-base font-normal'>No thanks, I’d rather not receive anything at all</label>
                         </div>
                     </div>
                 </div>
-                <div className="button text-center mb-[10px]">
-                    <button className='bg-color-1 font-mono text-base md:px-[40px] sm:px-[30px] px-[25px] md:py-[10px] py-[5px] border border-color-1 text-white rounded-full hover:bg-white hover:text-color-1' onClick={handleNextClick}>Next</button>
+                <div className="button text-center mb-[27px]">
+                    <button className='bg-color-1 font-mono text-base md:px-[40px] sm:px-[30px] px-[25px] md:py-[10px] py-[5px] border border-color-1 text-white rounded-full hover:bg-white hover:text-color-1' onClick={handleNextClick}>Reveal my report</button>
                 </div>
                 <div className="max-w-max mx-auto w-max">
                     <button className="border-b border-color-6 font-light w-max text-md lg:text-lg flex items-center gap-1" type='button' onClick={() => setStep(4)}><GoArrowLeft /> Back </button>
