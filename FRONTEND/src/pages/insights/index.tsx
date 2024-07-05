46cc2e5d1c679ff5dab3d854d72257c63040982e
Rajinder Singh <50661634+rajinderbarha@users.noreply.github.com>
2024-07-05 10:28:12 +0530

    remove meta from insight page

--- FRONTEND/src/pages/insights/index.tsx
+++ FRONTEND/src/pages/insights/index.tsx
@@ -1,17 +1,11 @@
 import Filters from "@/components/Insights/Filters";
 import { getInsightsData } from "../../../lib/sanity.query";
 import { FiltersType } from "../../../lib/interface";
-import Head from "next/head";
+
 
 const insights = ({ insights }: { insights: FiltersType[] }) => {
   return <>
-        <Head>
-          <title>Insights — Zest Digital</title>
-          <meta name="title" property="" content="Insights — Zest Digital" />
-          <meta name="url" property="og:url" content="https://www.zestdigital.com/insights" />
-          <meta name="keywords" property="og:keywords" content="zest, SEO, PPC, Growth, Company, CRO & Analytics, Landing Pages" />
-          <meta name="description" property="og:description" content="" />
-        </Head>
+
   
    <div className="Body_padding">
     <Filters data={insights[0]} />
