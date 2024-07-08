fd233be11f8f9c164dc4085e13bafbd64f1922fe
Rajinder Singh <50661634+rajinderbarha@users.noreply.github.com>
2024-07-08 09:16:07 +0530

    Update 404.tsx

--- FRONTEND/src/pages/404.tsx
+++ FRONTEND/src/pages/404.tsx
@@ -1,6 +1,6 @@
 import AOS from 'aos'
 import 'aos/dist/aos.css';
-import Head from 'next/head';
+
 import { useEffect } from 'react';
 
 export default function Custom404() {
@@ -11,9 +11,6 @@ export default function Custom404() {
     });
   }, []);
   return <>
-  <Head>
-  <title>404 | Page Not Found</title>
-  </ Head >
    <div className=" mb-28 overflow-hidden">
     <div className="flex items-center main_padding justify-center w-[100vw] h-[100vh] bg-color-1">
       <h1 className="text-[30px] font-mono font-semibold text-center lg:text-[50px] text-white" data-aos="zoom-in">404 | Page Not Found</h1>
