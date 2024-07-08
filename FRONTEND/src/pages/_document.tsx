6d484c164f460bd1d7c89ebf67db653bae194547
tejindersinghts75 <50661634+rajinderbarha@users.noreply.github.com>
2024-07-08 11:37:15 +0530

    undo the -document file

--- FRONTEND/src/pages/_document.tsx
+++ FRONTEND/src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="preload" href="../assets/images/compress/Gradient_v3_compress.avif" as="image"/>
-        <link rel="preload" href="../assets/IBMPlexMono/IBMPlexMono-SemiBold.ttf" as="font" type="font/ttf" crossOrigin="anonymous"/>
-      </Head>
+      <Head/>
 
       <body>
         <Main />
