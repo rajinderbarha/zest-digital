23c0427a15db1eb8d56524d3b37e1d8494ae2a49
tejindersinghts75 <50661634+rajinderbarha@users.noreply.github.com>
2024-07-08 17:54:15 +0530

    remove the all preloads from the _document.tsx

--- FRONTEND/src/pages/_document.tsx
+++ FRONTEND/src/pages/_document.tsx
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-        <Head>
-          <link
-            rel="preload"
-            href="/Gradient_v3_compress.avif"
-            as="image"
-          />
-        </Head>
+      <Head/>
 
       <body>
         <Main />
