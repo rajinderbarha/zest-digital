af5e563acf53da6b870f022ecb2f989bda342d35
tejindersinghts75 <50661634+rajinderbarha@users.noreply.github.com>
2024-08-16 10:38:24 +0530

    fix chat bot

--- FRONTEND/src/pages/_document.tsx
+++ FRONTEND/src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      {/* <Head /> */}
+      <Head>
+        <script
+          id="hs-script-loader"
+          async
+          defer
+          src="//js-eu1.hs-scripts.com/26290409.js"
+        ></script>
+      </Head>
 
       <body>
         <Main />
