640b85168fbcd9c763a4475136c048ae6ee2b1b3
tejindersinghts75 <50661634+rajinderbarha@users.noreply.github.com>
2024-08-16 16:17:25 +0530

    remove unwanted code after succesfuly embend the chat box

--- FRONTEND/src/pages/_document.tsx
+++ FRONTEND/src/pages/_document.tsx
@@ -3,15 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      {/* <Head /> */}
-      <Head>
-        {/* <script
-          id="hs-script-loader"
-          async
-          defer
-          src="//js-eu1.hs-scripts.com/26290409.js"
-        ></script> */}
-      </Head>
+      <Head />
 
       <body>
         <Main />
