427d195fb21d889e96bf964b15b4b4212a97f60a
tejindersinghts75 <50661634+rajinderbarha@users.noreply.github.com>
2024-08-16 10:11:03 +0530

    fix chat bot

--- FRONTEND/src/common/ChatBox/index.tsx
+++ FRONTEND/src/common/ChatBox/index.tsx
@@ -1,28 +1,46 @@
+// import { useEffect } from 'react';
+
+// const ChatBox: React.FC = () => {
+//     useEffect(() => {
+//         // Create and append the HubSpot script
+//         const script = document.createElement('script');
+//         script.src = '//js-eu1.hs-scripts.com/26290409.js';
+//         script.type = 'text/javascript';
+//         script.id = 'hs-script-loader';
+//         script.async = true;
+//         script.defer = true;
+
+//         // Append the script to the document head
+//         document.body.appendChild(script);
+
+//         // Cleanup function to remove the script if the component unmounts
+//         // return () => {
+//         //     const existingScript = document.getElementById('hs-script-loader');
+//         //     if (existingScript) {
+//         //         document.head.removeChild(existingScript);
+//         //     }
+//         // };
+//     }, []); // Empty dependency array ensures this effect runs once on mount and once on unmount
+
+//     return null; // This component doesn't render anything
+// };
+
+// export default ChatBox;
+
+
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 const ChatBox: React.FC = () => {
-    useEffect(() => {
-        // Create and append the HubSpot script
-        const script = document.createElement('script');
-        script.src = '//js-eu1.hs-scripts.com/26290409.js';
-        script.type = 'text/javascript';
-        script.id = 'hs-script-loader';
-        script.async = true;
-        script.defer = true;
-
-        // Append the script to the document head
-        document.body.appendChild(script);
-
-        // Cleanup function to remove the script if the component unmounts
-        // return () => {
-        //     const existingScript = document.getElementById('hs-script-loader');
-        //     if (existingScript) {
-        //         document.head.removeChild(existingScript);
-        //     }
-        // };
-    }, []); // Empty dependency array ensures this effect runs once on mount and once on unmount
-
-    return null; // This component doesn't render anything
+    return (
+        <>
+            <Script
+                id="hs-script-loader"
+                src="//js-eu1.hs-scripts.com/26290409.js"
+                strategy="lazyOnload"
+            />
+        </>
+    );
 };
 
 export default ChatBox;
